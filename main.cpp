20d42d21e4886709d1be33419be8a272073bacbe
Ivan Carcamo <ivandres73@unitec.edu>
2019-11-18 00:03:09 -0600

    convolution apparently done

--- main.cpp
+++ main.cpp
@@ -14,12 +14,15 @@ using std::array;
 
 #define IMG_SIZY 10
 #define IMG_SIZX 10
-
+#define KERNEL_X 3
+#define KERNEL_Y 3
+#define KERNEL_TOTAL 16
 array<short, 9> kernel= {1, 2, 1,
                          2, 4, 2,
                          1, 2, 1 };
 
-void convolution(short&, const array<short, 9>&, array<short, IMG_SIZX*IMG_SIZY>&);
+void convolution(short*, const array<short, 9>&, array<short, IMG_SIZX*IMG_SIZY>&);
+short kernel_calc(short *, short, short, const array<short, 9>&);
 
 int main() {
     CImg<short> image("square.jpg");
@@ -49,14 +52,37 @@ int main() {
     array<short, IMG_SIZX*IMG_SIZY> red;
     array<short, IMG_SIZX*IMG_SIZY> gre;
     array<short, IMG_SIZX*IMG_SIZY> blu;
-
+    cout << "new red matrix:\n";
+    convolution(ptr, kernel, red);
+    cout << "new green matrix:\n";
+    convolution(ptr, kernel, gre);
+    cout << "new blue matrix:\n";
+    convolution(ptr, kernel, blu);
     return 0;
 }
 
-void convolution(short& ptr, const array<short, 9>& kernel, array<short, IMG_SIZX*IMG_SIZY>& result) {
-    for (short i=0; i < IMG_SIZX; i++) {
-        for (short v=0; v < IMG_SIZY; v++) {
-            
+void convolution(short *ptr, const array<short, 9>& kernel, array<short, IMG_SIZX*IMG_SIZY>& result) {
+    short x_moves = IMG_SIZX - KERNEL_X;
+    short y_moves = IMG_SIZY - KERNEL_Y;
+    cout << "x_moves: " << x_moves << '\n';
+    for (short i=0; i <= x_moves; i++) {
+        for (short v=0; v <= y_moves; v++) {
+            cout << kernel_calc(ptr, i, v, kernel) << ',';
         }
+        cout << '\n';
     }
+}
+
+short kernel_calc(short *ptr, short x_offset, short y_offset, const array<short, 9>& kernel) {
+    short total_sum = 0;
+    short row_sum;
+    for (short i=0; i < 3; i++) {
+        row_sum = 0;
+        for (short v=0; v < 3; v++) {
+            row_sum += kernel[v*3 + i] * ptr[(i*10+x_offset) + (v+y_offset)];//operacion es columna x fila
+        }
+        total_sum += row_sum;
+    }
+    //cout << "total = " << total_sum << '\n';
+    return total_sum / KERNEL_TOTAL;
 }
